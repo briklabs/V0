dfcd70c00781ca126b8e770d0666306a5fae6ad8
Alex Valle <alex.valle@gmail.com>
2021-07-10 17:14:48 -0400

    working app; added rough draft for splash view

--- App.vue
+++ App.vue
@@ -1,17 +1,18 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+    <text class="text-color-primary">V0</text>
   </view>
 </template>
 
 <style>
 .container {
-  background-color: white;
+  background-color: gray;
   align-items: center;
   justify-content: center;
   flex: 1;
 }
 .text-color-primary {
-  color: blue;
+  color: white;
+  font-size: 48;
 }
 </style>
--- vueTransformerPlugin.js
+++ vueTransformerPlugin.js
@@ -1,34 +1,27 @@
-var semver = require('semver');
-var vueNativeScripts = require("vue-native-scripts");
+// For React Native version 0.59 or later
+var upstreamTransformer = require("metro-react-native-babel-transformer");
 
-var reactNativeVersionString = require("react-native/package.json").version;
+// You will need to use different transformers for different React Native versions
+// However, versions older than v0.59 are no longer supported by Vue Native
 
-var reactNativeMinorVersion = semver(reactNativeVersionString).minor;
+// For React Native version 0.56 - 0.58
+// var upstreamTransformer = require("metro/src/reactNativeTransformer");
 
-if (reactNativeMinorVersion >= 59) {
-  upstreamTransformer = require("metro-react-native-babel-transformer");
-} else if (reactNativeMinorVersion >= 56) {
-  upstreamTransformer = require("metro/src/reactNativeTransformer");
-} else if (reactNativeMinorVersion >= 52) {
-  upstreamTransformer = require("metro/src/transformer");
-} else if (reactNativeMinorVersion >= 47) {
-  upstreamTransformer = require("metro-bundler/src/transformer");
-} else if (reactNativeMinorVersion === 46) {
-  upstreamTransformer = require("metro-bundler/build/transformer");
-} else {
-  // handle RN <= 0.45
-  var oldUpstreamTransformer = require("react-native/packager/transformer");
-  upstreamTransformer = {
-    transform({ src, filename, options }) {
-      return oldUpstreamTransformer.transform(src, filename, options);
-    }
-  };
-}
+// For React Native version 0.52 - 0.55
+// var upstreamTransformer = require("metro/src/transformer");
+
+// For React Native version 0.47 - 0.51
+// var upstreamTransformer = require("metro-bundler/src/transformer");
+
+// For React Native version 0.46
+// var upstreamTransformer = require("metro-bundler/build/transformer");
+
+var vueNaiveScripts = require("vue-native-scripts");
 var vueExtensions = ["vue"]; // <-- Add other extensions if needed.
 
 module.exports.transform = function({ src, filename, options }) {
   if (vueExtensions.some(ext => filename.endsWith("." + ext))) {
-    return vueNativeScripts.transform({ src, filename, options });
+    return vueNaiveScripts.transform({ src, filename, options });
   }
   return upstreamTransformer.transform({ src, filename, options });
 };
