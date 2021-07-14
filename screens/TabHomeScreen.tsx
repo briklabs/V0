7e77e7d7043a0097d51b3d600c8e32c275df6d7a
Alex Valle <alex.valle@gmail.com>
2021-07-14 11:39:29 -0400

    added some basic elements to home screen

--- screens/TabHomeScreen.tsx
+++ screens/TabHomeScreen.tsx
@@ -6,7 +6,18 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>V0</Text>
+      <View style={styles.cardContainer}>
+        <Text style={styles.cardTitle}>Latest Session</Text>
+        <Text>Some session data here.</Text>
+      </View>
+      <View style={styles.cardContainer}>
+        <Text style={styles.cardTitle}>Recommended Workout</Text>
+        <Text>Maybe recommend a new workout routine.</Text>
+      </View>
+      <View style={styles.cardContainer}>
+        <Text style={styles.cardTitle}>Top Dawg</Text>
+        <Text>Show top 3 from the Leader Board</Text>
+      </View>
     </View>
   );
 }
@@ -14,11 +25,15 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
-  title: {
-    fontSize: 40,
+  cardContainer: {
+    padding: 20,
+  },
+  cardTitle: {
+    fontSize: 16,
     fontWeight: 'bold',
+    marginBottom: 4,
   },
 });
