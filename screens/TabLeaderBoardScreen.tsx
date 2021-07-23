61b36f3a0b20dd9577bf37d6706ab1bfa3d3c9c2
GraysonWest <graysonwest@gmail.com>
2021-07-23 08:49:17 -0400

    I beleive I fixed the issues

--- screens/TabLeaderBoardScreen.tsx
+++ screens/TabLeaderBoardScreen.tsx
@@ -12,7 +12,7 @@ export default class AvatarAndClickable extends Component {
     // simulate new users being added to leaderboard
     setInterval(() => {
       const newData = {
-        name: "New User Data!!",
+        name: "Gonna pay firebase!!",
         score: Math.floor(Math.random() * 100).toString(),
         iconUrl:
           "https://www.shareicon.net/data/128x128/2016/09/15/829473_man_512x512.png"
@@ -61,77 +61,26 @@ export default class AvatarAndClickable extends Component {
 
 const DATA = [
   {
-    name: "We Tu Lo",
+    name: "Grayson",
     score: null,
     iconUrl:
-      "https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59094043-stock-illustration-profile-icon-male-avatar.jpg"
+      "https://avatars.githubusercontent.com/u/55790741?s=120&v=4"
   },
   {
-    name: "Adam Savage",
+    name: "Alex",
     score: 12,
     iconUrl:
-      "https://www.shareicon.net/data/128x128/2016/09/15/829473_man_512x512.png"
+      "https://avatars.githubusercontent.com/u/20450617?s=64&v=4"
   },
   {
-    name: "Derek Black",
+    name: "Michael",
     score: 244,
-    iconUrl: "http://ttsbilisim.com/wp-content/uploads/2014/09/20120807.png"
+    iconUrl: "https://avatars.githubusercontent.com/u/4270658?s=64&v=4"
   },
   {
-    name: "Erika White",
+    name: "Som yong guy",
     score: 0,
     iconUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr27ZFBaclzKcxg2FgJh6xi3Z5-9vP_U1DPcB149bYXxlPKqv-"
   },
-  {
-    name: "Jimmy John",
-    score: 20,
-    iconUrl: "https://static.witei.com/static/img/profile_pics/avatar4.png"
-  },
-  {
-    name: "Joe Roddy",
-    score: 69,
-    iconUrl: "https://static.witei.com/static/img/profile_pics/avatar4.png"
-  },
-  {
-    name: "Ericka Johannesburg",
-    score: 101,
-    iconUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShPis8NLdplTV1AJx40z-KS8zdgaSPaCfNINLtQ-ENdPvrtMWz"
-  },
-  {
-    name: "Tim Thomas",
-    score: 41,
-    iconUrl: "http://ttsbilisim.com/wp-content/uploads/2014/09/20120807.png"
-  },
-  {
-    name: "John Davis",
-    score: 80,
-    iconUrl:
-      "https://cdn.dribbble.com/users/223408/screenshots/2134810/me-dribbble-size-001-001_1x.png"
-  },
-  {
-    name: "Tina Turner",
-    score: 22,
-    iconUrl:
-      "https://cdn.dribbble.com/users/223408/screenshots/2134810/me-dribbble-size-001-001_1x.png"
-  },
-  {
-    name: "Harry Reynolds",
-    score: null,
-    iconUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsSlzi6GEickw2Ft62IdJTfXWsDFrOIbwXhzddXXt4FvsbNGhp"
-  },
-  {
-    name: "Betty Davis",
-    score: 25,
-    iconUrl:
-      "https://landofblogging.files.wordpress.com/2014/01/bitstripavatarprofilepic.jpeg?w=300&h=300"
-  },
-  {
-    name: "Lauren Leonard",
-    score: 30,
-    iconUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr27ZFBaclzKcxg2FgJh6xi3Z5-9vP_U1DPcB149bYXxlPKqv-"
-  }
 ];
