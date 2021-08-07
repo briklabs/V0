9c1ff2e4f0d18c20c804739ad8d84ef0b355b3b5
Alex Valle <alex.valle@gmail.com>
2021-08-07 15:45:28 -0400

    implemented static dummy leaderboard

--- screens/TabLeaderBoardScreen.tsx
+++ screens/TabLeaderBoardScreen.tsx
@@ -1,42 +1,53 @@
-import * as React from 'react';
-import { StyleSheet, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, Alert } from 'react-native';
+import Leaderboard from 'react-native-leaderboard';
 
 import { View, Text } from '../components/Themed';
 
+interface BoardItemInterface {
+  name: string
+  score: number
+  iconUrl: string
+}
+
 export default function TabLeaderBoardScreen() {
+  const [data, setData] = useState(DATA)
+
+  useEffect(() => {
+    // simulate new users being added to leaderboard
+    setInterval(() => {
+      const newData: BoardItemInterface = {
+        name: 'Gonna pay firebase!!',
+        score: Math.floor(Math.random() * 100),
+        iconUrl:
+          'https://www.shareicon.net/data/128x128/2016/09/15/829473_man_512x512.png'
+      };
+      setData([...data, newData])
+    }, 3000);
+  })
+
+  function alert(title: string, body: string): void {
+    Alert.alert(title, body, [{ text: 'OK', onPress: () => { } }], {
+      cancelable: false
+    });
+  };
+
+  const props = {
+    labelBy: 'name',
+    sortBy: 'score',
+    data,
+    icon: 'iconUrl',
+    onRowPress: (item: BoardItemInterface, index: number) => {
+      alert(`${item.name} clicked`, `${item.score} points, wow!`)
+    },
+    evenRowColor: '#edfcf9'
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.boardTitle}>Bouldering</Text>
+      <Text style={styles.boardTitle}>Leaderboard</Text>
       <View style={styles.board}>
-        {
-          [1,2,3].map((_, index) => (
-            <View style={styles.userRow} key={index}>
-              <Image
-                style={styles.avatar}
-                source={{
-                  uri: 'https://randomuser.me/api/portraits/med/men/75.jpg'
-                }}
-              />
-              <Text>Alex</Text>
-            </View>
-          ))
-        }
-      </View>
-      <Text style={styles.boardTitle}>Top Rope (autobelay)</Text>
-      <View style={styles.board}>
-        {
-          [1,2,3].map((_, index) => (
-            <View style={styles.userRow} key={index}>
-              <Image
-                style={styles.avatar}
-                source={{
-                  uri: 'https://randomuser.me/api/portraits/med/men/75.jpg'
-                }}
-              />
-              <Text>MR. ROBOT</Text>
-            </View>
-          ))
-        }
+        <Leaderboard {...props} />
       </View>
     </View>
   );
@@ -65,3 +76,29 @@ const styles = StyleSheet.create({
     marginRight: 12,
   },
 });
+
+const DATA: BoardItemInterface[] = [
+  {
+    name: "Grayson",
+    score: 0,
+    iconUrl:
+      "https://avatars.githubusercontent.com/u/55790741?s=120&v=4"
+  },
+  {
+    name: "Alex",
+    score: 12,
+    iconUrl:
+      "https://avatars.githubusercontent.com/u/20450617?s=64&v=4"
+  },
+  {
+    name: "Michael",
+    score: 244,
+    iconUrl: "https://avatars.githubusercontent.com/u/4270658?s=64&v=4"
+  },
+  {
+    name: "Som yong guy",
+    score: 0,
+    iconUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr27ZFBaclzKcxg2FgJh6xi3Z5-9vP_U1DPcB149bYXxlPKqv-"
+  },
+];
